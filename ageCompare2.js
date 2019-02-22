4321bd0cb53b02b0c950f0b719929296911fa9e5
Naman Gulati <40243817+naman-gulati3@users.noreply.github.com>
2019-02-22 18:15:02 +0530

    Update ageCompare2.js

--- ageCompare2.js
+++ ageCompare2.js
@@ -2,7 +2,6 @@ function User (age,name){
 this.age = age;
 this.name = name;
 }
-var u1= new User();
 var u2 = new User();
 User.prototype.compare = function(u2){
 
@@ -14,4 +13,4 @@ console.log(u2.name + ' is older than '+ this.name);
 }
 var user1 = new User(21,'Naman');
 var user2 = new User(28,'xyz');
-user1.compare(user2);
+user2.compare(user1);
